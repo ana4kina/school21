6391780043c0b23a6b3dc4eb6cb1791f1e480005
lcombo <lcombo>
2025-08-28 08:33:35 +0300

    add Libft

---
+++ Libft/ft_split.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcombo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 21:15:22 by lcombo            #+#    #+#             */
+/*   Updated: 2021/10/23 21:16:27 by lcombo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int	ft_count(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
+		{
+			while (s[i] && s[i] != c)
+				i++;
+			count++;
+		}
+	}
+	return (count);
+}
+
+static char	**ft_mal(char **rtn, char const *s, char c)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
+		{
+			j = 0;
+			while (s[i + j] && s[i + j] != c)
+				j++;
+			rtn[count] = malloc(sizeof(char) * (j + 1));
+			i += j;
+			count++;
+		}
+	}
+	return (rtn);
+}
+
+static char	**ft_fill(char **rtn, char const *s, char c)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
+		{
+			j = 0;
+			while (s[i + j] && s[i + j] != c)
+			{
+				rtn[count][j] = (char)s[i + j];
+				j++;
+			}
+			rtn[count++][j] = '\0';
+			i += j;
+		}
+	}
+	return (rtn);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**rtn;
+	int		i;
+	int		size;
+
+	i = 0;
+	size = ft_count(s, c);
+	rtn = malloc(sizeof(char *) * (size + 1));
+	rtn[size] = 0;
+	rtn = ft_mal(rtn, s, c);
+	rtn = ft_fill(rtn, s, c);
+	return (rtn);
+}
---
+++ Libft/libft.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcombo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 16:43:12 by lcombo            #+#    #+#             */
+/*   Updated: 2021/10/23 21:40:55 by lcombo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int		ft_atoi(const char *str);
+int		ft_tolower(int ch);
+int		ft_toupper(int ch);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int		ft_memcmp(const void *buffer1, const void *buffer2, size_t count);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t number, size_t size);
+char	*ft_strdup(const char *str);
+void	*ft_memcpy(void *destination, const void *source, size_t n);
+void	*ft_memmove(void *destination, const void *source, size_t n);
+void	*ft_memchr(const void *buffer, int c, size_t count);
+void	*ft_memset(void *destination, int c, size_t n);
+char	*ft_strchr(const char *string, int symbol);
+char	*ft_strrchr(const char *string, int symbol);
+char	*ft_strstr(const char *strB, const char *strA);
+size_t	ft_strlen(const char *c);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_itoa(int n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+#endif
